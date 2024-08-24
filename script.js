277545ae0607700687d3d48205a7b8018c2ac9df
Aman <99051262+Aman96-star@users.noreply.github.com>
2024-08-24 23:20:32 +0530

    js

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-alert("loading www.Bookstore.com ....");
+// alert("loading www.Bookstore.com ....");
  
-var knowmore = document.getElementsByClassName("k-n");
-knowmore.click();
+// var knowmore = document.getElementsByClassName("k-n");
+// knowmore.click();
+
+
+// console.log(volkswagen)
+//
+
+// n()
+// function n(){
+//     console.log("h")
+// }
+
+let t =3
+
+function sq(num){
+        let ans =num*num
+        return ans
+}
+let sq0 =sq(t)
+let sq1 =sq(2)
+console.log(sq0)
+console.log(sq1)
